e61d6fa6df36aa112d38a4a61790d10aec930530
hks8900 <35518916+hks8900@users.noreply.github.com>
2019-07-01 13:27:58 +0900

    20190628_넥슨 입사 문제
    구조는  1~5000까지의 모든 값의 총 합이며, 모든 리스트  1 ~5000
    제네레이트 값 빼고 나머지를 셀프 넘버로 남긴다. 그 뒤 총 합 계산
    셀프 넘버값 1227365
    출처 - http://codingdojang.com/scode/365?answer_mode=hide

--- 20190628.py
+++ 20190628.py
@@ -7,33 +7,54 @@
 그런데 반대로, 제네레이터가 없는 숫자들도 있으며, 이런 숫자를
 인도의 수학자 Kaprekar가 셀프 넘버(self-number)라 이름 붙였다.
 예를 들어 1,3,5,7,9,20,31 은 셀프 넘버 들이다.
-
 1 이상이고 5000 보다 작은 모든 셀프 넘버들의 합을 구하라.
-
 """
-
 r = []
 l = []
+ll = []
 n = 0
 f = 0
 t = 0
-
-while t < 50:
+k = 0
+we = 0
+while t < 5000: # 임시로 50 설정
     l.append(0)
+    r.append(t)
     t += 1  # l 리스트 값 0으로 채워서 미리 칸 만듬. range out 에러 예방.
-while True:
-    n = int(input("입력"))
-    for ii in str(n):
+print(t)
+
+while n < 5000:
+
+    for ii in str(n): # 제네레이터 값 생성
         i = int(ii)
-        print(i)
+        #print(i)
         s = f + i
-        print("s",s)
+        #print("s",s)
         f = s
-        print("f", f)
+        #print("f", f)
         d = n + s
-        print(d)
+        #print(d)
         l[n] = d
+    d = 0
+    f = 0
+    n += 1
 
+for j in l: # ee는 제네 총함
+    ee=k + j
+    k= ee
+for jj in r: # 1 ~ 5000 값 총함
+    qw = we + jj
+    we = qw
 
-    print("list",l)
+print("list",format(l)) # 각 리스트 값 확인. 제네 값
+print("list",format(r)) # 1~5000
 
+te = list(set(r) - set(l)) # 겁색해보니 집합으로 변환 하여 차 집합 구함. 셀프 넘버 값
+te.sort()
+print(te)
+
+g = 0
+for h in te:
+    result = g + h
+    g = result
+print("셀프 넘버값",result) # 셀프 넘버 값 총 합
